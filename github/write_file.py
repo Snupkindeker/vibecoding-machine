133739663078e906bf136334bd45cf511eaee84f
Snupkindeker <mikhail.pdc144@gmail.com>
2026-08-25 12:50:44 +0300

    Updated write_file.py

--- github/write_file.py
+++ github/write_file.py
@@ -45,4 +45,4 @@ def write_file(repo: str, path: str, content: str, message: str = None) -> dict:
 
 
 if __name__ == "__main__":
-    write_file("Snupkindeker/vibecoding-machine", "eggs/test.txt", "print('Hi!'")
+    write_file("Snupkindeker/vibecoding-machine", "test.txt", "print('Hi!')")
