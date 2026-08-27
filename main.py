45d2148aea1fc1524bed6acbb4a3ccb6cde518d6
Snupkindeker <mikhail.pdc144@gmail.com>
2026-08-27 21:10:00 +0300

    Updated main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from ai import run_cycle
+from ai.make_context import make_context
+from ai.system_context import system_context
+
+
+def main():
+    messages = [system_context()]
+    prompt = ""
+    while True:
+        prompt = make_context("user", input("Type your message here: "))
+        if prompt['content'] == "/stop":
+            break
+        messages.append(prompt)
+        messages = run_cycle.run_cycle(messages)
