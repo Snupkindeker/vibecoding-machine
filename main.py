0d2c2df435cf0891b42ee8de535e5796d4de2366
Snupkindeker <mikhail.pdc144@gmail.com>
2026-08-27 21:11:36 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from ai.system_context import system_context
 
 def main():
     messages = [system_context()]
-    prompt = ""
     while True:
         prompt = make_context("user", input("Type your message here: "))
         if prompt['content'] == "/stop":
